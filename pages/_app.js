26283ff33770d949755d0bf76af485270b674b7e
Lucas-Palma <palmalucassantiago@gmail.com>
2023-03-23 18:26:01 -0300

    add: product details, context logic and cart logic

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import { Layout } from '../components';
 import React from 'react';
 import '../styles/globals.css';
+import { StateContext } from '../context/StateContext';
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }) {
-  return ( 
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,18 @@ import { Product, FooterBanner, HeroBanner } from '../components';
 const Home = ({ products, bannerData }) => (
   <div>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-    <div className='products-heading'>
+    <div className='products-heading'> 
       <h2>Best Selling Products</h2>
       <p>Speakers of many variations</p>
     </div>
-
     <div className='products-container'>
       {products?.map((product) => <Product key={product._id} product={product} />)}
     </div>
 
     <FooterBanner footerBanner={bannerData && bannerData[0]} />
-  </div>
+  </div>   
 );
-
+ 
 export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
   const products = await client.fetch(query);
